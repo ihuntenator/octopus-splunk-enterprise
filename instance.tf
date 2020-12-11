dd0e2ecee64f385452f0d4364ba27124f172ee68
Ian hunt <ihuntenator@gmail.com>
2020-12-12 11:13:35 +1300

    no vars at all

--- instance.tf
+++ instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "splunk" {
+  ami                    = "ami-085265c078d86c75b"
   subnet_id              = "subnet-74539012"
   availabilityZone       = "ap-southeast-2a"
   instance_type          = "t2.micro"
@@ -9,9 +10,5 @@ resource "aws_instance" "splunk" {
   }
   volume_tags = {
     Name = "splunk"
-  }
-  amis {
-    default = {
-      "ap-southeast-2" = "ami-085265c078d86c75b"
   }
 }
