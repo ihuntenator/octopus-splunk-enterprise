1830e054ceb80d44a4310e3e920e574651e34efc
Ian hunt <ihuntenator@gmail.com>
2020-12-12 11:18:40 +1300

    removed tags

--- instance.tf
+++ instance.tf
@@ -3,12 +3,4 @@ resource "aws_instance" "splunk" {
   subnet_id              = "subnet-74539012"
   availabilityZone       = "ap-southeast-2a"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.splunk_enterprise.id]
-
-  tags = {
-    Name = "splunk"
-  }
-  volume_tags = {
-    Name = "splunk"
-  }
 }
