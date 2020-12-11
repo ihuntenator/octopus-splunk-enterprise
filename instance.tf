2ea341b2bacb838c7209ee29c5198db7f77233bb
Ian hunt <ihuntenator@gmail.com>
2020-12-12 11:25:11 +1300

    removed availabilityZone

--- instance.tf
+++ instance.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "splunk" {
   ami                    = "ami-085265c078d86c75b"
   subnet_id              = "subnet-74539012"
-  availabilityZone       = "ap-southeast-2a"
   instance_type          = "t2.micro"
 }
